30d9786a33096572b08c17d23df1e37673068b19
tat2018 <39683675+tat2018@users.noreply.github.com>
2018-06-17 14:37:39 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+document.getElementById("calculate-button").onclick = function() {
+	//Coletar números dentro das caixas de input
+	let elem1 = document.getElementById('elem-1');
+	let elem2 = document.getElementById('elem-2');
+
+	let val1 = parseInt(elem1.value);
+	let val2 = parseInt(elem2.value);
+
+	let choice = document.querySelector( 'input[name="operation"]:checked');
+	let operation = choice.value;
+	let operationResult = getResult(val1, val2, operation);
+
+	let resultElement = document.getElementById( 'result' );
+    resultElement.innerHTML = operationResult;
+}
+
+/**
+* Returns the result of an operation over two numbers.
+* Arguments: two valid numbers and operation symbol.
+* Supports sum, subtraction, multiplication and division.
+*/
+
+function getResult(val1, val2, operation) {
+    if (operation === '+') {
+    	return val1 + val2;
+    } else if (operation === '-') {
+    	return val1 - val2;
+    } else if (operation === '*') {
+    	return val1 * val2;
+    } else if (operation === '/') {
+    	return val1 / val2;
+    }
+}
